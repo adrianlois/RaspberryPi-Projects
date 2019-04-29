57f26b23b54d5a635b9399f6dcafaef7af2520f1
Adrian Lois <1989adrianlois@gmail.com>
2019-04-29 21:07:22 +0200

    thingspeak_raspi_dht22.py

---
+++ thingspeak_raspi_dht22.py
@@ -0,0 +1,27 @@
+import sys
+import RPi.GPIO as GPIO
+#from time import sleep
+import Adafruit_DHT
+import urllib2
+miAPIWrite = "XXXXXXXXXXXXXXXX"
+raspiGPIO = "X"
+def getSensorData():
+   RH, T = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, raspiGPIO)
+   return (str(RH), str(T))
+def main():
+   print 'starting...'
+   baseURL = 'https://api.thingspeak.com/update?api_key=%s' % miAPIWrite
+   while True:
+       try:
+           RH, T = getSensorData()
+           f = urllib2.urlopen(baseURL +
+                               "&field1=%s&field2=%s" % (RH, T))
+           print f.read()
+           f.close()
+           sleep(5)
+       except:
+           print 'exiting.'
+           break
+# call main
+if __name__ == '__main__':
+   main()
